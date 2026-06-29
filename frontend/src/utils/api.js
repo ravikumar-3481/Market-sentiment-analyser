b78489cb36d259380c8e23aa9fe4b6695867e941
Ravi Kumar Vishwakarma <ravivish968@gmail.com>
2026-06-29 07:22:25 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -5,6 +5,7 @@
 
 export const DEFAULT_API_HOST = 'https://market-sentiment-analyser-1.onrender.com';
 
+
 export const getApiSettings = () => {
   const host = localStorage.getItem('marketpulse_api_host') || DEFAULT_API_HOST;
   return { host };
